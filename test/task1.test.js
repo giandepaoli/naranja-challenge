575034f31dc9fdfde72e5a544cbc139fb51a8cb2
Gianfranco <sistemas@ecoautoparts.com.ar>
2022-03-31 03:12:44 -0300

    Refactor folders

--- test/task1.test.js
+++ test/task1.test.js
@@ -1,13 +1,13 @@
-const task_1 = require(".")
+const task1 = require("../task1")
 
 describe("Finding prime numbers until certain value", () => {
   test("Prime numbers until 30 should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]", () => {
-    expect(task_1(30)).toStrictEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+    expect(task1(30)).toStrictEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
   })
   test("Prime numbers until 5 should return [2, 3]", () => {
-    expect(task_1(5)).toStrictEqual([2, 3])
+    expect(task1(5)).toStrictEqual([2, 3])
   })
   test("Prime numbers until 0 should return []", () => {
-    expect(task_1(0)).toStrictEqual([])
+    expect(task1(0)).toStrictEqual([])
   })
 })
